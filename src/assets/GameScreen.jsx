e5d38553ddabfb761ab8f1d2864475cb4e0d8cd8
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-02-21 22:31:51 +0530

    added galaxy background in gamescreen

--- src/assets/GameScreen.jsx
+++ src/assets/GameScreen.jsx
@@ -4,6 +4,7 @@ import { GameOverScreen } from "./GameOverScreen.jsx";
 import { PauseMenuScreen } from "./PauseMenuScreen.jsx";
 import { StartGame } from './scripts/startGame.jsx'
 import { playerId } from "../components/configs.js";
+import Galaxy from "./Galaxy/Galaxy.jsx";
 
 export function GameScreen({ canvasRef, difficulty, gameState }) {
     const [isPaused, setIsPaused] = useState(false)
@@ -38,7 +39,7 @@ export function GameScreen({ canvasRef, difficulty, gameState }) {
                 console.log('commencing drag...')
                 nodesRef.current.forEach(node => {
                     if (Math.hypot(node.x - x, node.y - y) < node.radius * 1.5 && node.owner === playerId) {
-                        if (!dragSelectedRef.current.includes(node)) {dragSelectedRef.current.push(node)}
+                        if (!dragSelectedRef.current.includes(node)) { dragSelectedRef.current.push(node) }
                     }
                 })
             }
@@ -72,17 +73,39 @@ export function GameScreen({ canvasRef, difficulty, gameState }) {
 
     return (
         <>
-            <canvas ref={canvasRef} />
-            <div style={{ position: 'absolute', top: 0, left: 0 }} >
-                {!isPaused
-                    ? <PauseButton setIsPaused={setIsPaused} />
-                    : <PauseMenuScreen setIsPaused={setIsPaused} />
-                }
+            <div>
+                <div className="absolute w-full h-full z-0" >
+                    <Galaxy
+                        mouseRepulsion
+                        mouseInteraction={false}
+                        density={1}
+                        glowIntensity={0.2}
+                        saturation={0}
+                        hueShift={140}
+                        twinkleIntensity={0.3}
+                        rotationSpeed={0.1}
+                        repulsionStrength={2}
+                        autoCenterRepulsion={0}
+                        starSpeed={0.5}
+                        speed={1}
+                    />
+                </div>
+
+                <canvas ref={canvasRef} className="absolute top-0 left-0 z-10" />
+
+                <div className="relative top-0 left-0 z-20" >
+                    {!isPaused
+                        ? <PauseButton setIsPaused={setIsPaused} />
+                        : <PauseMenuScreen setIsPaused={setIsPaused} />
+                    }
+                </div>
+
+
+                {/* GAME OVER SCREEN */}
+                <div className="z-30">
+                    <GameOverScreen />
+                </div>
             </div>
-
-
-            {/* GAME OVER SCREEN */}
-            <GameOverScreen />
         </>
     )
 }
