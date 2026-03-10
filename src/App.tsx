cea1b7cff932351a21477670225b6ee985645fda
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-03-10 14:26:48 +0530

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { Homepage } from "./assets/HomePage"
-import { GameScreen } from "./assets/GameScreen"
+import { lazy, Suspense } from 'react';
 import { useGameContext } from "./assets/GameContext"
 
+const Homepage = lazy(() => import('./assets/HomePage').then(module => ({ default: module.Homepage })));
+const GameScreen = lazy(() => import('./assets/GameScreen').then(module => ({ default: module.GameScreen })));
 
 function App() {
   const { gameState } = useGameContext()
 
   return (
-    <div>
-      {gameState === 'menu'
-        ? <Homepage />
-        : <GameScreen />
-      }
-    </div>
+    <Suspense fallback={<div className="w-full h-full flex items-center justify-center bg-gray-900 text-white">Loading...</div>}>
+      <div>
+        {gameState === 'menu'
+          ? <Homepage />
+          : <GameScreen />
+        }
+      </div>
+    </Suspense>
   )
 }
 
